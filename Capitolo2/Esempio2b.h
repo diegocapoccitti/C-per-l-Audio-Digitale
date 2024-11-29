ae8eddc45e946f477fbeb884c161dc136e767a7f
diegocapoccitti <diego.capoccitti@gmail.com>
2024-11-29 18:20:22 +0100

    cap2

---
+++ Capitolo2/Esempio2b.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//Questo è un commento
+
+int main()
+{
+    std::cout << "Hello World!"; //Questo è un altro commento
+
+/* Questo è un
+   commento
+   Su più righe
+   */
+}
